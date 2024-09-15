87d7b3e1fd50201b0f6fd05b4b08fb0692f6ba3d
Kyren223 <ulmliad223@gmail.com>
2024-09-15 20:25:49 +0300

    feat: disable hardtime

--- .config/nvim/lua/custom/plugins/hardtime.lua
+++ .config/nvim/lua/custom/plugins/hardtime.lua
@@ -1,16 +1,16 @@
 return {
-    'm4xshen/hardtime.nvim',
-    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-    event = { 'BufReadPre', 'BufNewFile' },
-    keys = {
-        { '<leader>ht', '<<cmd>Hardtime toggle<cr>', desc = '[H]ardtime [T]oggle' },
-        { '<leader>hT', '<<cmd>Hardtime report<cr>', desc = '[H]ardtime Report' },
-    },
-    opts = {
-        restriction_mode = 'hint',
-        restricted_keys = {
-            ['h'] = {},
-            ['l'] = {},
-        },
-    },
+    -- 'm4xshen/hardtime.nvim',
+    -- dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    -- event = { 'BufReadPre', 'BufNewFile' },
+    -- keys = {
+    --     { '<leader>ht', '<<cmd>Hardtime toggle<cr>', desc = '[H]ardtime [T]oggle' },
+    --     { '<leader>hT', '<<cmd>Hardtime report<cr>', desc = '[H]ardtime Report' },
+    -- },
+    -- opts = {
+    --     restriction_mode = 'hint',
+    --     restricted_keys = {
+    --         ['h'] = {},
+    --         ['l'] = {},
+    --     },
+    -- },
 }
