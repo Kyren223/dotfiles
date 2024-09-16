6a5ed8d9472541ddc646c852e2963c4fbfd8a2a8
Kyren223 <ulmliad223@gmail.com>
2024-09-16 13:39:21 +0300

    feat: added render markdown plugin

---
+++ .config/nvim/lua/custom/plugins/render-markdown.lua
@@ -0,0 +1,8 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    event = 'VeryLazy',
+    opts = {
+        sign = { enabled = false },
+    },
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+}
