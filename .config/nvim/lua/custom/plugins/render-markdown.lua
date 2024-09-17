60576b1e0be6de84844790e3ab00b65887602ce4
Kyren223 <ulmliad223@gmail.com>
2024-09-17 11:22:21 +0300

    feat: configured render-markdown

--- .config/nvim/lua/custom/plugins/render-markdown.lua
+++ .config/nvim/lua/custom/plugins/render-markdown.lua
@@ -1,8 +1,16 @@
 return {
     'MeanderingProgrammer/render-markdown.nvim',
-    event = 'VeryLazy',
+    event = { 'BufReadPre', 'BufNewFile' },
     opts = {
         sign = { enabled = false },
+        bullet = { icons = { '', '◆' } },
+        checkbox = {
+            -- unchecked = { icon = '󰄱 ' },
+            -- checked = { icon = '󰄲 ' },
+            unchecked = { icon = '󰄰 ' },
+            checked = { icon = '󰄳 ' },
+            custom = { todo = { rendered = '◯ ' } },
+        },
     },
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
 }
