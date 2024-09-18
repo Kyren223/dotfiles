e0334d2271bb6bc91590e888a86bcfedb6b58a55
Kyren223 <ulmliad223@gmail.com>
2024-09-18 13:18:59 +0300

    feat: changed markdown tab indent from 4 to 2

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,9 @@
 Event = 'VeryLazy'
+
+vim.opt.tabstop = 2 -- number of visual spaces per TAB
+vim.opt.softtabstop = 2 -- number of idfk tab when editing
+vim.opt.shiftwidth = 2 -- number of spaces to insert on tab
+
 vim.opt.wrap = true
 vim.opt.breakindent = true
 vim.opt.linebreak = true
