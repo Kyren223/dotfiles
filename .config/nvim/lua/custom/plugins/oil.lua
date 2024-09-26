072382200256ca103c518a0f31aed1df0415b025
Kyren223 <ulmliad223@gmail.com>
2024-09-26 16:52:58 +0300

    feat: removed lazy loading from oil to not have loading issues

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -1,12 +1,7 @@
 return {
     'stevearc/oil.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    keys = {
-        { '<leader>of', '<cmd>Oil<cr>', desc = '[O]il [F]ilesystem' },
-        { '<leader>oF', '<cmd>lua require("oil").toggle_float<cr>', desc = '[O]il [F]ilesystem (float)' },
-        { '<leader>fo', '<cmd>Oil<cr>', desc = '[F]ilesystem [O]il' },
-        { '<leader>fO', '<cmd>lua require("oil").toggle_float<cr>', desc = '[F]ilesystem [O]il (float)' },
-    },
+    keys = { { '<leader>fo', '<cmd>Oil<cr>', desc = '[F]ilesystem [O]il' } },
     opts = {
         default_file_explorer = true,
         skip_confirm_for_simple_edits = true,
@@ -19,7 +14,7 @@ return {
             ['<M-h>'] = 'actions.select_split',
             ['<M-v>'] = 'actions.select_vsplit',
             ['<C-r>'] = 'actions.refresh',
-            ['<C-q>'] = 'actions.close',
+            ['Q'] = 'actions.close',
             ['gS'] = 'actions.change_sort',
         },
         view_options = {
@@ -28,9 +23,5 @@ return {
         },
     },
     cmd = 'Oil',
-    event = function()
-        if vim.fn.isdirectory(vim.fn.expand('%')) == 1 then
-            return 'VimEnter'
-        end
-    end,
+    event = 'VimEnter',
 }
