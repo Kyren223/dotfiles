963c9f303d63af242216f1f2f9c29e9151ddbdcc
Kyren223 <ulmliad223@gmail.com>
2024-09-28 18:04:04 +0300

    feat(treesitter): not sure what this does

--- .config/nvim/lua/custom/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/custom/plugins/nvim-treesitter.lua
@@ -10,6 +10,11 @@ return {
                 auto_install = true,
                 highlight = { enable = true },
                 indent = { enable = true },
+                rainbow = {
+                    enable = true,
+                    extended_mode = true,
+                    max_file_lines = nil,
+                },
             })
         end,
     },
