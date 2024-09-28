374372d226530d1f522c3d44eb6e83346433624e
Kyren223 <ulmliad223@gmail.com>
2024-09-28 18:04:22 +0300

    feat(vim-illuminate): added back

--- .config/nvim/lua/custom/plugins/vim-illuminate.lua
+++ .config/nvim/lua/custom/plugins/vim-illuminate.lua
@@ -1,9 +1,7 @@
 return {
-    -- -- TODO: Make sure it uses a diff color
-    -- -- the color rn is too similar to visual mode
-    -- 'RRethy/vim-illuminate',
-    -- event = 'LspAttach',
-    -- keys = {
-    --     { '<leader>it', '<cmd>IlluminateToggle<cr>', desc = '[I]lluminate [Tloggle]' },
-    -- },
+    'RRethy/vim-illuminate',
+    event = 'LspAttach',
+    keys = {
+        { '<leader>it', '<cmd>IlluminateToggle<cr>', desc = '[I]lluminate [Tloggle]' },
+    },
 }
