65c25a0e1dad56a53394b55a9b19e2ca7c3c7016
Kyren223 <ulmliad223@gmail.com>
2024-09-29 13:16:21 +0300

    feat: added snippet for carbonight group

--- .config/nvim/lua/custom/snippets/lua.lua
+++ .config/nvim/lua/custom/snippets/lua.lua
@@ -9,8 +9,28 @@ local function s(abbr, expansion, args)
     return ls.snippet(abbr, require('luasnip.extras.fmt').fmt(expansion, args))
 end
 
+local carbonight_group = [=[
+--[[
+Copyright (C) 2024 Kyren223
+This file is licensed under the GPL-3.0-or-later license, see https://fsf.org/licenses/gpl-3.0
+--]]
+
+local utils = require("carbonight.utils")
+
+local M = {{}}
+
+function M.get(c)
+    return {{
+        {}
+    }}
+end
+
+return M
+]=]
+
 ls.add_snippets('lua', {
     s('desc', "{{ desc = '{}' }}", { i(0) }),
     s('cmd', '<cmd>{}<cr>', { i(0) }),
     s('scmd', "'<cmd>{}<cr>'", { i(0) }),
+    s('cng', carbonight_group, { i(0) }),
 })
