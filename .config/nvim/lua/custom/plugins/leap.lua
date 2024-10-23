7f4aba8a5bfab0edb30c8dab2dd2aec63afe6443
Kyren223 <ulmliad223@gmail.com>
2024-10-24 00:29:15 +0300

    feat: change nvim-surround keymaps to default, leap keymaps changed too

--- .config/nvim/lua/custom/plugins/leap.lua
+++ .config/nvim/lua/custom/plugins/leap.lua
@@ -1,18 +1,15 @@
 return {
     'ggandor/leap.nvim',
+    lazy = false,
     dependencies = { 'tpope/vim-repeat' },
-    keys = {
-        { 's', '<Plug>(leap)', mode = { 'n', 'x', 'o' } },
-        { 'S', '<Plug>(leap-from-window)' },
-        {
-            'gs',
-            function()
-                require('leap.remote').action()
-            end,
-        },
-    },
     config = function()
         require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
         vim.api.nvim_set_hl(0, 'LeapBackdrop', { fg = '#777777' })
+
+        vim.keymap.set({ 'n', 'x' }, 's', '<Plug>(leap)')
+        vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+        vim.keymap.set('n', 's', require('leap.remote').action)
+        vim.keymap.set('o', 'z', '<Plug>(leap-forward)')
+        vim.keymap.set('o', 'Z', '<Plug>(leap-backward)')
     end,
 }
