f590351dd0a3a4706452723e5a0422a25a08121f
Kyren223 <Kyren223@proton.me>
2024-11-24 12:44:45 +0200

    Added desktop file for whatsapp and sorted out the JDK thing

--- hosts/laptop-nixos/home.nix
+++ hosts/laptop-nixos/home.nix
@@ -51,22 +51,22 @@
     libnotify
     wl-clipboard
     openssl
-    whatsapp-for-linux
     whatsie
     libratbag
     piper
     goose
     sqlc
     jetbrains.idea-community
-    jdk17
     jdk
     maven
     gradle
+    kdePackages.filelight
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".jdks/openjdk17".source = pkgs.openjdk17;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -82,6 +82,21 @@
   home.sessionPath = [
     "$HOME/.local/bin"
   ];
+
+  xdg.desktopEntries = {
+    "com.ktechpit.whatsie" = {
+      name = "WhatsApp";
+      genericName = "Watsie, unofficial WhatsApp client";
+      exec = "whatsie %U";
+      terminal = false;
+      icon = "com.github.eneshecan.WhatsAppForLinux";
+      categories = [ "Application" "Network" "WebBrowser" ];
+      settings = {
+        Keywords = "chat;im;messaging;messenger;sms;whatsapp;whatsapp-desktop;whatsie;";
+      };
+    };
+  };
+
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
