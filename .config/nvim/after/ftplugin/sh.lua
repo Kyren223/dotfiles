97669867212df2f5a3314db6abac2b09a02bd5fb
Kyren223 <Kyren223@proton.me>
2024-11-28 11:26:03 +0200

    Bash/sh now have 2 space indents

---
+++ .config/nvim/after/ftplugin/sh.lua
@@ -0,0 +1,5 @@
+Event = 'VeryLazy'
+
+vim.opt_local.tabstop = 2 -- number of visual spaces per TAB
+vim.opt_local.softtabstop = 2 -- number of idfk tab when editing
+vim.opt_local.shiftwidth = 2 -- number of spaces to insert on tab
