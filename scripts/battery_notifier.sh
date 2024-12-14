32af9e531b168d431fb37afe13823ce13928a8bf
Kyren223 <Kyren223@proton.me>
2024-12-14 22:42:24 +0200

    Made battery at 80% a critical notif, might revert cuz critical breaks the automatic 5s dismiss, and closing manually is a pain

--- scripts/battery_notifier.sh
+++ scripts/battery_notifier.sh
@@ -6,7 +6,7 @@ battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
 battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
 if [ "$battery_status" = "Charging" ] && [ "$battery_level" -ge 80 ]; then
-    /etc/profiles/per-user/kyren/bin/notify-send --urgency=normal \
+    /etc/profiles/per-user/kyren/bin/notify-send --urgency=critical \
       --app-name="Battery Notifier" \
       --expire-time=5000 \
       "Battery at $battery_level%, unplug!"
