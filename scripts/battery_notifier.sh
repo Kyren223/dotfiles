87a5338df794cde74b7d96f99a365356ff1dfc56
Kyren223 <Kyren223@proton.me>
2024-12-22 15:27:33 +0200

    Fixed battery script

--- scripts/battery_notifier.sh
+++ scripts/battery_notifier.sh
@@ -1,19 +1,16 @@
-set -eu
 export DISPLAY=:0.0
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
 
 battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
 battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
-if [ "$battery_status" = "Charging" ] && [ "$battery_level" -ge 80 ]; then
-    /etc/profiles/per-user/kyren/bin/notify-send --urgency=normal \
+if [ "$battery_status" != "Discharging" ] && [ "$battery_level" -ge 90 ]; then
+    /etc/profiles/per-user/kyren/bin/notify-send --urgency=critical \
       --app-name="Battery Notifier" \
       --expire-time=5000 \
       "Battery at $battery_level%, unplug!"
-fi
-
-if [ "$battery_status" = "Charging" ] && [ "$battery_level" -ge 90 ]; then
-    /etc/profiles/per-user/kyren/bin/notify-send --urgency=critical \
+elif [ "$battery_status" != "Discharging" ] && [ "$battery_level" -ge 80 ]; then
+    /etc/profiles/per-user/kyren/bin/notify-send --urgency=normal \
       --app-name="Battery Notifier" \
       --expire-time=5000 \
       "Battery at $battery_level%, unplug!"
