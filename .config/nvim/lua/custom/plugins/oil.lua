9745e7004f4a74b55c5fc1cdd12df2ee7cfc38e4
Kyren223 <Kyren223@proton.me>
2025-04-23 18:30:00 +0300

    Oil can now copy paste into clipboard with gy and gp

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -17,10 +17,22 @@ return {
             ['<C-r>'] = 'actions.refresh',
             ['Q'] = 'actions.close',
             ['gS'] = 'actions.change_sort',
+            ['gy'] = 'actions.copy_to_system_clipboard',
+            ['gp'] = 'actions.paste_from_system_clipboard',
         },
         view_options = {
             show_hidden = true,
             natural_order = true,
+        },
+
+        lsp_file_methods = {
+            -- Enable or disable LSP file operations
+            enabled = true,
+            -- Time to wait for LSP file operations to complete before skipping
+            timeout_ms = 1000,
+            -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+            -- Set to "unmodified" to only save unmodified buffers
+            autosave_changes = false,
         },
     },
     cmd = 'Oil',
