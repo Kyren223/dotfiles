9a53ba17c03978069ac26598a21961d69da69e58
Kyren223 <Kyren223@proton.me>
2025-05-01 16:52:23 +0300

    Fixed harpoon keybinds of 6+ not working unless already loaded

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -2,7 +2,19 @@ return {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = { '<C-e>', '<leader>a', '<leader>1', '<leader>2', '<leader>3', '<leader>4', '<leader>5' },
+    keys = {
+        '<C-e>',
+        '<leader>a',
+        '<leader>1',
+        '<leader>2',
+        '<leader>3',
+        '<leader>4',
+        '<leader>5',
+        '<leader>6',
+        '<leader>7',
+        '<leader>8',
+        '<leader>9',
+    },
     config = function()
         local harpoon = require('harpoon')
         harpoon:setup()
