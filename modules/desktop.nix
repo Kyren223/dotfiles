90c63def40f1cf89b106448a80997834d44c56a0
Kyren223 <Kyren223@proton.me>
2025-05-25 19:53:07 +0300

    Fixed graphics reset issue due to nvidia GPU

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -9,4 +9,9 @@
     phoronix-test-suite
   ];
 
+  # Fix "graphics reset" issue with nvidia GPU
+  # See section 10 of https://wiki.archlinux.org/title/NVIDIA/Tips_and_tricks
+  boot.extraModprobeConfig = ''
+    options nvidia  NVreg_PreserveVideoMemoryAllocations=1
+  '';
 }
