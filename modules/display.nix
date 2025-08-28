5036afc864ad7cc38781f1a826633a087feaa58d
Kyren223 <contact@kyren.codes>
2025-08-28 15:16:15 +0300

    Removed KDE related display stuff

--- modules/display.nix
+++ modules/display.nix
@@ -13,15 +13,15 @@
   #     User = "kyren";
   #   };
   # };
-
+  #
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
+  # services.xserver.enable = true;
+  #
   # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+  # services.xserver.xkb = {
+  #   layout = "us";
+  #   variant = "";
+  # };
 
 }
