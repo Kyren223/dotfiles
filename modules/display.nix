494abaa6a29ded81a6f4ddf7594f231667a27c4d
Kyren223 <contact@kyren.codes>
2025-08-30 15:27:40 +0300

    Re-enabled KDE

--- modules/display.nix
+++ modules/display.nix
@@ -41,24 +41,24 @@ in {
   # Optional, hint electron apps to use wayland:
   # environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
-  # # Enable the KDE Plasma Desktop Environment.
-  # services.displayManager.sddm.enable = true;
-  # services.desktopManager.plasma6.enable = true;
-  # services.displayManager.sddm.settings = {
-  #   Autologin = {
-  #     Session = "plasma.desktop";
-  #     User = "kyren";
-  #   };
-  # };
-  #
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.settings = {
+    Autologin = {
+      Session = "plasma.desktop";
+      User = "kyren";
+    };
+  };
+
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  # services.xserver.enable = true;
-  #
+  services.xserver.enable = true;
+
   # Configure keymap in X11
-  # services.xserver.xkb = {
-  #   layout = "us";
-  #   variant = "";
-  # };
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 
 }
