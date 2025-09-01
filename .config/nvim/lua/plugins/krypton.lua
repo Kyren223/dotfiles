cf153aa6b06a7e8fe248631aa0a23fcdf4c75a65
Kyren223 <contact@kyren.codes>
2025-09-01 21:33:55 +0300

    Add Krypton language

---
+++ .config/nvim/lua/plugins/krypton.lua
@@ -0,0 +1,28 @@
+return {
+    dir = '~/projects/krypton',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    lazy = false,
+    init = function() end,
+    config = function()
+        vim.filetype.add({
+            extension = {
+                kr = 'krypton',
+            },
+        })
+        require('nvim-web-devicons').set_icon({
+            kr = {
+                icon = '', -- 
+                color = '#07f7f7',
+                -- color = '#2c638a',
+                -- color = '#37b1ce',
+                name = 'Krypton',
+            },
+        })
+
+        local icon, color = require('nvim-web-devicons').get_icon('main.kr', 'kr', { default = true })
+        -- vim.notify('ICON: ' .. icon)
+        -- vim.notify('COLOR: ' .. color)
+        assert(icon == '')
+        assert(color == 'DevIconKrypton')
+    end,
+}
