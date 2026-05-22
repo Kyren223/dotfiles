4453a4406d38697141e7fb9be7abb31860900480
Kyren223 <contact@kyren.codes>
2026-05-22 16:13:17 +0300

    trying to fix session issues

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -6,5 +6,17 @@ return {
         session_lens = { load_on_setup = false },
         bypass_save_filetypes = { 'snacks_dashboard', 'terminal' },
         close_filetypes_on_save = { 'checkhealth', 'build_terminal' },
+        pre_save_cmds = {
+            function()
+                for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                    if vim.bo[buf].buftype == 'terminal' then
+                        vim.api.nvim_buf_delete(buf, { force = true })
+                    end
+                    if vim.bo[buf].filetype == 'build_terminal' then
+                        vim.api.nvim_buf_delete(buf, { force = true })
+                    end
+                end
+            end,
+        },
     },
 }
