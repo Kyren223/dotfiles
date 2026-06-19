c5eef73c549cbc5605f62fe80668af37ced69d06
Kyren223 <contact@kyren.codes>
2026-06-19 21:06:48 +0300

    Fix lua globals not loading, I now get autocompletion for stuff like vim, Snacks, os, tonumber etc

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -4,6 +4,7 @@ return {
     cmd = {
         'lua-language-server',
         '--logpath=' .. vim.fn.expand('~/.cache/nvim/lua-language-server/log'),
+        '--metapath=' .. vim.fn.expand('~/.cache/nvim/lua-language-server/meta'),
     },
     filetypes = { 'lua' },
     root_markers = {
@@ -15,5 +16,23 @@ return {
         'selene.toml',
         'selene.yml',
         '.git',
+    },
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server you're targetting Neovim's runtime environment
+                version = 'LuaJIT',
+                -- CRITICAL: Allows modern lua_ls to parse the dynamic third-party paths
+                -- injected by lazydev without strict root containment rules.
+                pathStrict = false,
+            },
+            workspace = {
+                -- Stops the server from asking to configure your workspace environment reactively
+                checkThirdParty = false,
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
     },
 }
